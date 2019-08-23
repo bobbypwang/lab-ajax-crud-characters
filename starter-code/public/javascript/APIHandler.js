1265f5b6c4d037631e383918036d5942a35475f1
Bobby Wang <bobby.p.wang@gmail.com>
2019-08-23 16:01:37 -0400

    Created the API handlers needed to access data.

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,93 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
+    axios.get(`${this.BASE_URL}/characters`)
+      .then(res => {
 
+        console.log(res)
+
+        document.querySelector(".characters-container").innerHTML = ""
+
+        for (let i=0;i < res.data.length;i++) {
+          let charBox = `<div class="character-info">
+          <div class="id">Id: ${res.data[i].id}</div>
+          <div class="name">Name: ${res.data[i].name}</div>
+          <div class="occupation">Occupation: ${res.data[i].occupation}</div>
+          <div class="cartoon">Cartoon: ${res.data[i].cartoon}</div>
+          <div class="weapon">Weapon: ${res.data[i].weapon}</div>
+          </div>`
+        
+        document.querySelector(".characters-container").innerHTML += charBox
+        }
+      })
+      .catch(err => {
+        document.querySelector(".characters-container").innerHTML = `<div class="character-info">Unable to fetch all.<br />${err}</div>`
+      })
   }
 
-  getOneRegister () {
+  getOneRegister(idSearch) {
+    axios.get(`${this.BASE_URL}/characters/${idSearch}`)
+      .then(res => {
 
+        let charBox = `<div class="character-info">
+        <div class="id">Id: ${res.data.id}</div>
+        <div class="name">Name: ${res.data.name}</div>
+        <div class="occupation">Occupation: ${res.data.occupation}</div>
+        <div class="cartoon">Cartoon: ${res.data.cartoon}</div>
+        <div class="weapon">Weapon: ${res.data.weapon}</div>
+        </div>`
+
+        document.querySelector(".characters-container").innerHTML = charBox
+
+      })
+      .catch(err => {
+        document.querySelector(".characters-container").innerHTML = `<div class="character-info">Unable to pull ID: ${idSearch}.<br />${err}</div>`
+      })
+  } 
+
+  deleteOneRegister(idSearch) {
+    axios.delete(`${this.BASE_URL}/characters/${idSearch}`)
+    .then(res => {
+      document.querySelector(".characters-container").innerHTML = `<div class="character-info">${res.data.name} (ID: ${idSearch}) has been deleted.</div>`
+      document.querySelector("#delete-one").setAttribute("style", "background-color: green;");
+    })
+    .catch(err => {
+      document.querySelector(".characters-container").innerHTML = `<div class="character-info">Unable to delete ID: ${idSearch}.<br/>${err}</div>`
+      document.querySelector("#delete-one").setAttribute("style", "background-color: red;");
+    })
   }
 
-  createOneRegister () {
+  createOneRegister(charData) {
+    axios.post(`${this.BASE_URL}/characters/`, charData)
 
+    .then(res => {
+      if (charData.cartoon) {
+        document.querySelector(".characters-container").innerHTML = `<div class="character-info">Cartoon character ${charData.name} the ${charData.occupation} carryinng a ${charData.weapon} has been created.</div>`
+      } else {
+        document.querySelector(".characters-container").innerHTML = `<div class="character-info">${charData.name} the ${charData.occupation} carryinng a ${charData.weapon} has been created.</div>`
+      }     
+      document.querySelector("#new-character-form #send-data").setAttribute("style", "background-color: green;");
+    })
+    .catch(err => {
+      document.querySelector(".characters-container").innerHTML = `<div class="character-info">${err}</div>`
+      document.querySelector("#new-character-form #send-data").setAttribute("style", "background-color: red;");
+    })
   }
 
-  updateOneRegister () {
+  updateOneRegister(charData) {
+    axios.patch(`${this.BASE_URL}/characters/${charData.id}`, charData)
 
+      .then(res => {
+        document.querySelector(".characters-container").innerHTML = `<div class="character-info">(ID:${charData.id}) has been updated.<br/></div>`
+        document.querySelector("#edit-character-form #send-data").setAttribute("style", "background-color: green;");
+      })
+      .catch(err => {
+        document.querySelector(".characters-container").innerHTML = `<div class="character-info">Error<br />>${err}</div>`
+        document.querySelector("#edit-character-form #send-data").setAttribute("style", "background-color: red;");
+      })
   }
 
-  deleteOneRegister () {
-
-  }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,44 @@
 const charactersAPI = new APIHandler("http://localhost:8000")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById('fetch-all').onclick = function () {
+    charactersAPI.getFullList()
+  }
 
+  document.getElementById('fetch-one').onclick = function () {
+    charactersAPI.getOneRegister(document.querySelector("body > section:nth-child(1) > section > div:nth-child(2) > input[type=text]").value)
+  }
+
+  document.getElementById('delete-one').onclick = function () {
+    charactersAPI.deleteOneRegister(document.querySelector("body > section:nth-child(1) > section > div.operation.delete > input[type=text]").value)
+  }
+
+  document.getElementById('edit-character-form').onsubmit = function (e) {
+    e.preventDefault()
+
+    let charData= {
+      id: document.querySelector("#edit-character-form > div:nth-child(1) > input[type=text]").value,
+      name: document.querySelector("#edit-character-form > div:nth-child(2) > input[type=text]").value,      
+      occupation: document.querySelector("#edit-character-form > div:nth-child(3) > input[type=text]").value,
+      weapon: document.querySelector("#edit-character-form > div:nth-child(4) > input[type=text]").value,
+      cartoon: document.querySelector("#edit-character-form > div:nth-child(5) > input[type=checkbox]").checked
+    }
+
+    charactersAPI.updateOneRegister(charData)
+  }
+
+
+  document.getElementById('new-character-form').onsubmit = function (e) {
+    e.preventDefault()
+
+    let charData = {
+      name: document.querySelector("#new-character-form > div:nth-child(1) > input[type=text]").value,
+      occupation: document.querySelector("#new-character-form > div:nth-child(2) > input[type=text]").value,
+      weapon: document.querySelector("#new-character-form > div:nth-child(3) > input[type=text]").value,
+      cartoon: document.querySelector("#new-character-form > div:nth-child(4) > input[type=checkbox]").checked
+    }
+
+    charactersAPI.createOneRegister(charData)
   }
   
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
 })
